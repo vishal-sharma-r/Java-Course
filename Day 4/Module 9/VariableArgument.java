18dc50ddf1e135b4793e9874c111a23d0d0ecd7c
“vishal-sharma-r” <sharmaji20rattan@gmail.com>
2024-01-25 18:05:24 +0530

    done

--- Day 4/Module 9/VariableArgument.java
+++ Day 4/Module 9/VariableArgument.java
@@ -9,6 +9,6 @@ public class VariableArgument {
         argument(1,2,3,4,5,6,7,8,9,10,11,12);
         int a[] = {1,2,3,4};
         argument(a);
-        argument(new int[]{9,8,7,6,5,4,3});
+        argument(new int[]{9,8,7,6,5,4,5});
     }
 }
